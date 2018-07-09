563adb1c5d56f9b6dbfa7806506fdb6e2b34f708
zgy <765395183@qq.com>
2018-07-09 17:22:25 +0800

    0709

---
+++ promotion/cmake-build-debug/CMakeFiles/promotion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/promotion.dir/main.cpp.obj"
+  "promotion.pdb"
+  "promotion.exe"
+  "promotion.exe.manifest"
+  "libpromotion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/promotion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ promotion/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    unsigned u1 = 42, u2 = 10;
+    cout << u2 - u1 << endl;
+
+    cout << sizeof(double);
+
+    return 0;
+}
